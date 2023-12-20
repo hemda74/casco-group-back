b9108208954589b0e26aacbcee3e1028921f1bad
hemda74 <92821656+hemda74@users.noreply.github.com>
2023-12-20 15:10:11 +0200

    add arabic conetnt

--- app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/components/columns.tsx
@@ -1,15 +1,16 @@
-"use client"
+"use client";
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from "@tanstack/react-table";
 
-import { CellAction } from "./cell-action"
+import { CellAction } from "./cell-action";
 
 export type ColorColumn = {
-  id: string
+  id: string;
   name: string;
+  name_ar: string;
   value: string;
   createdAt: string;
-}
+};
 
 export const columns: ColumnDef<ColorColumn>[] = [
   {
@@ -22,9 +23,12 @@ export const columns: ColumnDef<ColorColumn>[] = [
     cell: ({ row }) => (
       <div className="flex items-center gap-x-2">
         {row.original.value}
-        <div className="h-6 w-6 rounded-full border" style={{ backgroundColor: row.original.value }} />
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.value }}
+        />
       </div>
-    )
+    ),
   },
   {
     accessorKey: "createdAt",
@@ -32,6 +36,6 @@ export const columns: ColumnDef<ColorColumn>[] = [
   },
   {
     id: "actions",
-    cell: ({ row }) => <CellAction data={row.original} />
+    cell: ({ row }) => <CellAction data={row.original} />,
   },
 ];
