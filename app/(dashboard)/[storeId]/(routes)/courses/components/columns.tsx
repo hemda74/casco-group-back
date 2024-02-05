8b5cc85c15ab7d0a0372b6f86b71d34c746e76f8
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-06 01:31:51 +0200

    update data in columun

--- app/(dashboard)/[storeId]/(routes)/courses/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/courses/components/columns.tsx
@@ -1,62 +1,127 @@
-"use client"
+'use client';
 
-import { ColumnDef } from "@tanstack/react-table"
+import { ColumnDef } from '@tanstack/react-table';
 
-import { CellAction } from "./cell-action"
+import { CellAction } from './cell-action';
 
 export type ProductColumn = {
-  id: string
-  name: string;
-  price: string;
-  category: string;
-  size: string;
-  color: string;
-  createdAt: string;
-  isFeatured: boolean;
-  isArchived: boolean;
-}
+	id: string;
+	name: string;
+	price: string;
+	category: string;
+	name_ar: string;
+	intro: string;
+	intro_ar: string;
+	duaration: string;
+	duration_ar: string;
+	who_sh_att: string;
+	who_sh_att_ar: string;
+	c_obje: string;
+	c_obje_ar: string;
+	c_content: string;
+	c_content_ar: string;
+	wh_we_bnfi: string;
+	wh_we_bnfi_ar: string;
+	c_in_house: string;
+	c_in_house_ar: string;
+	delv_and_leaders: string;
+	delv_and_leaders_ar: string;
+	date_and_rev: string;
+	date_and_rev_ar: string;
+};
 
 export const columns: ColumnDef<ProductColumn>[] = [
-  {
-    accessorKey: "name",
-    header: "Name",
-  },
-  {
-    accessorKey: "isArchived",
-    header: "Archived",
-  },
-  {
-    accessorKey: "isFeatured",
-    header: "Featured",
-  },
-  {
-    accessorKey: "price",
-    header: "Price",
-  },
-  {
-    accessorKey: "category",
-    header: "Category",
-  },
-  {
-    accessorKey: "size",
-    header: "Size",
-  },
-  {
-    accessorKey: "color",
-    header: "Color",
-    cell: ({ row }) => (
-      <div className="flex items-center gap-x-2">
-        {row.original.color}
-        <div className="h-6 w-6 rounded-full border" style={{ backgroundColor: row.original.color }} />
-      </div>
-    )
-  },
-  {
-    accessorKey: "createdAt",
-    header: "Date",
-  },
-  {
-    id: "actions",
-    cell: ({ row }) => <CellAction data={row.original} />
-  },
+	{
+		accessorKey: 'name',
+		header: 'Name',
+	},
+	{
+		accessorKey: 'name_ar',
+		header: 'Arabic Name',
+	},
+	{
+		accessorKey: 'intro',
+		header: 'Introduction',
+	},
+	{
+		accessorKey: 'intro_ar',
+		header: 'Introduction in Arabic',
+	},
+	{
+		accessorKey: 'duaration',
+		header: 'Course duration in Arabic',
+	},
+	{
+		accessorKey: 'duaration_ar',
+		header: 'Course duration in Arabic',
+	},
+	{
+		accessorKey: 'who_sh_att',
+		header: 'who_should_attend this course',
+	},
+	{
+		accessorKey: 'who_sh_att_ar',
+		header: 'who_should_attend this course in Arabic',
+	},
+	{
+		accessorKey: 'c_obje',
+		header: 'course objective',
+	},
+	{
+		accessorKey: 'c_obje_ar',
+		header: 'course objective in Arabic',
+	},
+	{
+		accessorKey: 'c_content',
+		header: 'course Content',
+	},
+	{
+		accessorKey: 'c_content_ar',
+		header: 'course Content in Arabic',
+	},
+	{
+		accessorKey: 'wh_we_bnfi',
+		header: 'what we benefit',
+	},
+	{
+		accessorKey: 'wh_we_bnfi_ar',
+		header: 'what we benefit in Arabic',
+	},
+	{
+		accessorKey: 'c_in_house',
+		header: 'Course in house',
+	},
+	{
+		accessorKey: 'c_in_house_ar',
+		header: 'Course in house in Arabic',
+	},
+	{
+		accessorKey: 'delv_and_leaders',
+		header: 'Style of Delivery and Course Leaders',
+	},
+	{
+		accessorKey: 'delv_and_leaders_ar',
+		header: 'Style of Delivery and Course Leaders in Arabic',
+	},
+	{
+		accessorKey: 'price',
+		header: 'Price',
+	},
+	{
+		accessorKey: 'category',
+		header: 'Category',
+	},
+
+	{
+		accessorKey: 'date_and_rev',
+		header: 'Date',
+	},
+	{
+		accessorKey: 'date_and_rev_ar',
+		header: 'Date',
+	},
+	{
+		id: 'actions',
+		cell: ({ row }) => <CellAction data={row.original} />,
+	},
 ];
--- app/(dashboard)/[storeId]/(routes)/courses/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/courses/page.tsx
@@ -23,9 +23,26 @@ const ProductsPage = async ({ params }: { params: { storeId: string } }) => {
 		id: item.id,
 		name: item.name,
 		name_ar: item.name_ar,
-
+		intro: item.intro,
+		intro_ar: item.intro_ar,
+		duaration: item.duaration,
+		duration_ar: item.duration_ar,
+		who_sh_att: item.who_sh_att,
+		who_sh_att_ar: item.who_sh_att_ar,
+		c_obje: item.c_obje,
+		c_obje_ar: item.c_obje_ar,
+		c_content: item.c_content,
+		c_content_ar: item.c_content_ar,
+		wh_we_bnfi: item.wh_we_bnfi,
+		wh_we_bnfi_ar: item.wh_we_bnfi_ar,
+		c_in_house: item.c_in_house,
+		c_in_house_ar: item.c_in_house_ar,
+		delv_and_leaders: item.delv_and_leaders,
+		delv_and_leaders_ar: item.delv_and_leaders_ar,
 		price: formatter.format(item.price.toNumber()),
 		category: item.category.name,
+		date_and_rev: format(item.date_and_rev, 'MMMM do, yyyy'),
+		date_and_rev_ar: format(item.date_and_rev_ar, 'MMMM do, yyyy'),
 	}));
 
 	return (
