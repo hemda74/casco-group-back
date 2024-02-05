bbb18644f30733b204ec186cc0692298ad5c8dff
Ahmed Hemdan <92821656+hemda74@users.noreply.github.com>
2024-02-05 23:14:44 +0200

    remove un used satatisics

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -17,10 +17,10 @@ interface DashboardPageProps {
 }
 
 const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
-	const totalRevenue = await getTotalRevenue(params.storeId);
-	const graphRevenue = await getGraphRevenue(params.storeId);
-	const salesCount = await getSalesCount(params.storeId);
-	const stockCount = await getStockCount(params.storeId);
+	// const totalRevenue = await getTotalRevenue(params.storeId);
+	// const graphRevenue = await getGraphRevenue(params.storeId);
+	// const salesCount = await getSalesCount(params.storeId);
+	// const stockCount = await getStockCount(params.storeId);
 
 	return (
 		<div className="flex-col">
