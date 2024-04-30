92df34547ed66d3e08544bd37c70d8c3a058f27f
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-04-30 14:54:03 +0300

    comment unused import

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -5,9 +5,9 @@ import { Overview } from '@/components/overview';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Heading } from '@/components/ui/heading';
 // import { getTotalRevenue } from '@/actions/get-total-revenue';
-import { getSalesCount } from '@/actions/get-sales-count';
+// import { getSalesCount } from '@/actions/get-sales-count';
 // import { getGraphRevenue } from '@/actions/get-graph-revenue';
-import { getStockCount } from '@/actions/get-stock-count';
+// import { getStockCount } from '@/actions/get-stock-count';
 import { formatter } from '@/lib/utils';
 
 interface DashboardPageProps {
