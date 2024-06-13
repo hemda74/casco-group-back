08d96d68dd436183f9a85f69db8b4a1a8bdd9c9f
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-06-13 17:39:49 +0300

    adding craetion date on service module and handling table to dispaly services

--- app/(dashboard)/[storeId]/(routes)/services/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/services/components/columns.tsx
@@ -8,8 +8,14 @@ export type ServiceColumn = {
 	name: string;
 	name_ar: string;
 	category: string;
-	experts: string;
-
+	experts: string[];
+	title: string[];
+	title_ar: string[];
+	desc_1: string[];
+	desc_2: string[];
+	desc_1_ar: string[];
+	desc_2_ar: string[];
+	createdAt: string;
 };
 
 export const columns: ColumnDef<ServiceColumn>[] = [
@@ -26,10 +32,42 @@ export const columns: ColumnDef<ServiceColumn>[] = [
 		header: 'Category___________________________________',
 	},
 	{
-		accessorKey: 'experts',
-		header: 'Experts___________________________________',
+		id: 'experts',
+		header: 'experts___________________________________',
+		cell: ({ row }) => row.original.experts.join('___________________________________'),
 	},
 	{
+		id: 'title',
+		header: 'Title_in_English___________________________________',
+		cell: ({ row }) => row.original.title.join('___________________________________'),
+	}, {
+		id: 'desc_1',
+		header: 'First_Description_in_English___________________________________',
+		cell: ({ row }) => row.original.desc_1.join('___________________________________'),
+	}, {
+		id: 'desc_2',
+		header: 'Second_Description_in_English___________________________________',
+		cell: ({ row }) => row.original.desc_2.join('___________________________________'),
+	},
+	{
+		id: 'title_ar',
+		header: 'Title_in_Arabic___________________________________',
+		cell: ({ row }) => row.original.title_ar.join('___________________________________'),
+	}, {
+		id: 'desc_1_ar',
+		header: 'First_Description_in_Arabic___________________________________',
+		cell: ({ row }) => row.original.desc_1_ar.join('___________________________________'),
+	}, {
+		id: 'desc_2_ar',
+		header: 'Second_Description_in_Arabic___________________________________',
+		cell: ({ row }) => row.original.desc_2_ar.join('___________________________________'),
+	},
+	{
+		accessorKey: 'createdAt',
+		header: 'Date',
+	},
+	{
+		accessorKey: 'Actions',
 		id: 'actions',
 		cell: ({ row }) => <CellAction data={row.original} />,
 	},
--- app/(dashboard)/[storeId]/(routes)/services/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/services/page.tsx
@@ -1,7 +1,7 @@
 import prismadb from '@/lib/prismadb';
-
 import { ServiceClient } from './components/client';
 import { ServiceColumn } from './components/columns';
+import { format } from 'date-fns';
 
 const ServicesPage = async ({ params }: { params: { storeId: string } }) => {
 	const services = await prismadb.service.findMany({
@@ -19,7 +19,7 @@ const ServicesPage = async ({ params }: { params: { storeId: string } }) => {
 			},
 		},
 		orderBy: {
-			name: 'desc',
+			createdAt: 'desc',
 		},
 	});
 
@@ -28,8 +28,14 @@ const ServicesPage = async ({ params }: { params: { storeId: string } }) => {
 		name: item.name,
 		name_ar: item.name_ar,
 		category: item.category.name,
-		experts: item.expertServices.map(es => es.expert.name).join(',--------------- '), // expert 'name' field
-
+		experts: item.expertServices.map(es => es.expert.name), // expert 'name' field
+		title: item.serviceDesc.map(ti => ti.title),
+		desc_1: item.serviceDesc.map(des1 => des1.desc_1),
+		desc_2: item.serviceDesc.map(des1 => des1.desc_2),
+		title_ar: item.serviceDescAr.map(ti_ar => ti_ar.title_ar),
+		desc_1_ar: item.serviceDescAr.map(des1_ar => des1_ar.desc_1_ar),
+		desc_2_ar: item.serviceDescAr.map(des1_ar => des1_ar.desc_2_ar),
+		createdAt: format(item.createdAt, 'MMMM do, yyyy'),
 	}));
 
 	return (
