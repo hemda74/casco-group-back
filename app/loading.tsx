5fc85a66d0e4cd4f3adcd0da69448539258b3746
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-06-15 03:07:37 +0300

    .

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,11 +4,11 @@ import { Loader } from "@/components/ui/loader";
 
 
 const Loading = () => {
-  return ( 
-    <div className="flex h-full w-full items-center justify-center">
+  return (
+    <div className="flex h-full w-full items-center justify-center ">
       <Loader />
     </div>
-   );
+  );
 }
- 
+
 export default Loading;
