19c624241ccbbda8af184214842c8d09d705391b
Ahmed Hemdan <ahmedashrafhemdan74@gmail.com>
2024-07-09 02:29:14 +0300

    replace services category with new category

--- app/(dashboard)/[storeId]/(routes)/news-categories/[categoryId]/components/category-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/news-categories/[categoryId]/components/category-form.tsx
@@ -7,7 +7,7 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 import { Trash } from 'lucide-react';
-import { ServicesCategory } from '@prisma/client';
+import { NewsCategory } from '@prisma/client';
 import { useParams, useRouter } from 'next/navigation';
 
 import { Input } from '@/components/ui/input';
@@ -33,7 +33,7 @@ const formSchema = z.object({
 type CategoryFormValues = z.infer<typeof formSchema>;
 
 interface CategoryFormProps {
-	initialData: ServicesCategory | null;
+	initialData: NewsCategory | null;
 
 }
 
